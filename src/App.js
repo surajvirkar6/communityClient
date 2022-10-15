e9177eb1272ecfc7bae80bb57e35634d63969d0d
Suraj Virkar <87970303+surajvirkar6@users.noreply.github.com>
2022-10-15 21:57:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+
+function App() {
+  return (
+    <div>
+      <Register />
+      {/* <Login /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Register/Register.js
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+function Register() {
+  const [name, setname] = useState("");
+  const [mobile, setmobile] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+  const [errorText, seterrorText] = useState("")
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    isValid();
+  }
+
+  function isValid(){
+    if(!name || name.match(/[^A-Za-z]/)){
+      seterrorText("Please enter valid name");
+      return false;
+    } else if(!mobile || mobile.toString().length !== 10){
+      seterrorText("Please enter 10 digit mobile number");
+      return false;
+    } else if(!email){
+      seterrorText("Please enter valid email");
+      return false;
+    } else if(!password){
+      seterrorText("Please enter valid password");
+      return false;
+    } else if(!confirmPassword){
+      seterrorText("Please enter valid confirm password");
+      return false;
+    }
+
+    seterrorText('');
+    return true;
+  }
+
+  return (
+    <div>
+      <div>
+        <h3>Register</h3>
+        <p>{errorText}</p>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setname(e.target.value)}
+              placeholder="Enter your Name"
+            />
+            <input
+              type="number"
+              value={mobile}
+              onChange={(e) => setmobile(e.target.value)}
+              placeholder="Enter your Mobile Number"
+            />
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setemail(e.target.value)}
+              placeholder="Enter your Email"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setpassword(e.target.value)}
+              placeholder="Enter your Password"
+            />
+            <input
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setconfirmPassword(e.target.value)}
+              placeholder="Enter yout Confirm Password"
+            />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
